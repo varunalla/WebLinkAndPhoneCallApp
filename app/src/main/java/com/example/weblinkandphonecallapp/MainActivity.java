d2a5bac0d769187b44e20f6c08654687843a3ae7
Varun Alla <varun.alla@gmail.com>
2022-03-13 23:56:07 -0700

    Updated code for url empty validation Added phone validation and attached on click event Updated on click event for close app functionality

--- app/src/main/java/com/example/weblinkandphonecallapp/MainActivity.java
+++ app/src/main/java/com/example/weblinkandphonecallapp/MainActivity.java
@@ -1,53 +1,90 @@
 package com.example.weblinkandphonecallapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.service.autofill.RegexValidator;
+import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int CALL_PHONE_PERMISSION=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Web Link and Phone Call Activity");
     }
+
     @Override
     protected void onStart() {
         super.onStart();
+        int callPhonePerm=ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE);
+        if(callPhonePerm!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.CALL_PHONE},CALL_PHONE_PERMISSION);
+        }
     }
+
     @Override
     protected void onRestart() {
         super.onRestart();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
     }
+
     @Override
     protected void onStop() {
         super.onStop();
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
-    void openBroswer(View view){
-        EditText urlEditText=(EditText) findViewById(R.id.editTextUrl);
-        String url=urlEditText.getText().toString();
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(Uri.parse(url));
-        startActivity(i);
+    public void closeApp(View view){
+    finish();
     }
-    void openDialer(View view){
+    public void openBrowser(View view) {
+        EditText urlEditText = (EditText) findViewById(R.id.editTextUrl);
+        String url = urlEditText.getText().toString();
+        if(!url.isEmpty()){
+            if(!url.startsWith("http")) url="https://"+url;
+            Uri targetPage = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, targetPage);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "Please Enter a URL", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    public void openDialer(View view) {
+        EditText urlEditText = (EditText) findViewById(R.id.editTextPhone);
+        String phoneNumber = urlEditText.getText().toString();
+        if(PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber)){
+
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "Please Enter a valid phone number", Toast.LENGTH_SHORT).show();
+        }
     }
 }
